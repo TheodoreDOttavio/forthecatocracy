990d11d71b4f30be8bd2532991b38da1cceb2bea
teddottavio <teddottavio@yahoo.com>
2017-09-27 13:36:15 -0400

    poking around at the javascript options

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ A straightforward site to promote artisan woodworking peices.
 
 No shopping cart. Single purchases, no quantity, and low volume.
 No MVC framework. Very few pages, and no need for a database.
+
+## to add products:
+/assets/photos/[product id # yymmdd[2 digit sequence]]/*-thumb.jpg
+/assets/photos/[product id # yymmdd[2 digit sequence]]/*.jpg
+...or a json file with subfolder description and price?
--- assets/javascripts/catocracy.js
+++ assets/javascripts/catocracy.js
@@ -1,15 +1,31 @@
-var pdfFilesDirectory = '/uploads/pdf/';
-
-// get auto-generated page
-$.ajax({url: pdfFilesDirectory}).then(function(html) {
-    // create temporary DOM element
-    var document = $(html);
-
-    // find all links ending with .pdf
-    document.find('a[href$=.pdf]').each(function() {
-        var pdfName = $(this).text();
-        var pdfUrl = $(this).attr('href');
-
-        // do what you want here
-    })
+$(document).on('page:load', function() {
 });
+
+$("#myproducts").html(getProducts());
+console.log('response: ');
+console.log(getProducts());
+
+function getProducts(){
+  var jpgFilesDirectory = '/assets/photos/2017050800/';
+  var outputHtml = 'test';
+
+  //----ajax is not a function? ok....
+
+  // // get auto-generated page
+  // $.ajax({url: jpgFilesDirectory}).then(function(html) {
+  //     // create temporary DOM element
+  //     var document = $(html);
+  //
+  //     // find all links ending with .jpg
+  //     document.find('a[href$=.jpg]').each(function() {
+  //         var jpgName = $(this).text();
+  //         var jpgUrl = $(this).attr('href');
+  //
+  //         // do what you want here
+  //         outputHtml += jpgName + "<br>";
+  //     })
+  // });
+
+  return outputHtml;
+
+};
