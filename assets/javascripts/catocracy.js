37f4ca32ef8e2f18516cfc9ca61a10909d219572
teddottavio <teddottavio@yahoo.com>
2017-09-28 02:27:33 -0400

    refactoring the header

--- assets/javascripts/catocracy.js
+++ assets/javascripts/catocracy.js
@@ -1,31 +1,33 @@
-$(document).on('page:load', function() {
+$(document).ready(function(){
+  $("#header").load("_navigation.html");
 });
 
-$("#myproducts").html(getProducts());
-console.log('response: ');
-console.log(getProducts());
 
-function getProducts(){
-  var jpgFilesDirectory = '/assets/photos/2017050800/';
-  var outputHtml = 'test';
-
-  //----ajax is not a function? ok....
-
-  // // get auto-generated page
-  // $.ajax({url: jpgFilesDirectory}).then(function(html) {
-  //     // create temporary DOM element
-  //     var document = $(html);
-  //
-  //     // find all links ending with .jpg
-  //     document.find('a[href$=.jpg]').each(function() {
-  //         var jpgName = $(this).text();
-  //         var jpgUrl = $(this).attr('href');
-  //
-  //         // do what you want here
-  //         outputHtml += jpgName + "<br>";
-  //     })
-  // });
-
-  return outputHtml;
-
-};
+// // $(document).("#myproducts").html(getProducts());
+// console.log('response: ');
+// console.log(getProducts());
+//
+// function getProducts(){
+//   var jpgFilesDirectory = '/assets/photos/2017050800/';
+//   var outputHtml = 'test';
+//
+//   //----ajax is not a function? ok....
+//
+//   // get auto-generated page
+//   $.ajax({url: jpgFilesDirectory}).then(function(html) {
+//       // create temporary DOM element
+//       var document = $(html);
+//
+//       // find all links ending with .jpg
+//       document.find('a[href$=.jpg]').each(function() {
+//           var jpgName = $(this).text();
+//           var jpgUrl = $(this).attr('href');
+//
+//           // do what you want here
+//           outputHtml += jpgName + "<br>";
+//       })
+//   });
+//
+//   return outputHtml;
+//
+// };
