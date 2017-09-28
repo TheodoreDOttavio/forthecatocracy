a5e793d5822ac2a3e98df531e6debcaffe3b7f78
teddottavio <teddottavio@yahoo.com>
2017-09-28 16:48:51 -0400

    placeholder site building warning

--- index.php
+++ index.php
@@ -27,7 +27,8 @@
 
     <!-- Content Body -->
     <div class="container">
-      <img src="assets/images/header.jpg" alt=""></img>
+      <!-- <img src="assets/images/header.jpg" alt=""></img> -->
+      <h1>Site Building in Progress!</h1>
       These designs came about after making a few cat scratching posts for my friends. We wanted all natural fibers instead of carpeting. Next we tipped the post itself back and curved it like a tree base to help your home owner reach out. It would be great to attach a cat scratching post to the wall, but this wasn't an option for us. For that reason, the legs form a wider base than many posts on the market to provide a sturdy clawing. Finally the construction follows classic furniture design for added strength and long life. I attached the rope with simple U-shaped nails and no glue so that it can be replaced, and I keep the rear legs flush to the back wall in case your space is limited and you need to sneak them behind another piece of furniture. I hope your kitties will enjoy the look of finished wood in their home!
     </div>
 
