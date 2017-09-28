43b3560b4a44d1b8061b5bd240b344aaa13dd1c5
teddottavio <teddottavio@yahoo.com>
2017-09-28 12:16:11 -0400

    adding php buildpack for heroku

--- assets/javascripts/catocracy.js
+++ assets/javascripts/catocracy.js
@@ -1,32 +1,47 @@
 $(document).ready(function(){
   $("#header").load("_navigation.html");
+  // getProducts();
 });
 
-
-// // $(document).("#myproducts").html(getProducts());
-// console.log('response: ');
-// console.log(getProducts());
-//
 // function getProducts(){
-//   var jpgFilesDirectory = '/assets/photos/2017050800/';
-//   var outputHtml = 'test';
+//   // var jpgFilesDirectory = '/assets/photos/2017050800/';
+//   var jpgFilesDirectory = '_navigation.html';
+//   var outputHtml = 'ok...';
 //
-//   //----ajax is not a function? ok....
+//   // // outputHtml = jQuery.get({url: jpgFilesDirectory, data: data});
+//   // $.get( jpgFilesDirectory, function( data ) {
+//   //   outputHtml = data;
+//   //   // $( ".result" ).html( data );
+//   //   alert( "Load was performed." );
+//   // });
 //
-//   // get auto-generated page
-//   $.ajax({url: jpgFilesDirectory}).then(function(html) {
-//       // create temporary DOM element
-//       var document = $(html);
-//
-//       // find all links ending with .jpg
-//       document.find('a[href$=.jpg]').each(function() {
-//           var jpgName = $(this).text();
-//           var jpgUrl = $(this).attr('href');
-//
-//           // do what you want here
-//           outputHtml += jpgName + "<br>";
-//       })
+//   $.get({
+//     url: '/assets/photos/2017050800/',
+//     success: function(data){
+//       alert( "Load was performed." );
+//       $(data).find("a:contains(.jpg)").each(function(){
+//         // will loop through
+//         var images = $(this).attr("href");
+//         $('#myproducts').html(images);
+//         // $('<p></p>').html(images).appendTo('#myproducts')
+//       });
+//     }
 //   });
+//
+//   // // get auto-generated page
+//   // $.ajax({url: jpgFilesDirectory}).then(function(data) {
+//   //   // create temporary DOM element
+//   //   var doc = $(data);
+//   //
+//   //   // find all links ending with .jpg
+//   //   doc.find('a[href$=.jpg]').each(function() {
+//   //     var jpgName = $(this).text();
+//   //     var jpgUrl = $(this).attr('href');
+//   //
+//   //     // do what you want here
+//   //     outputHtml += jpgName + "<br>";
+//   //   });
+//   // });
 //
 //   return outputHtml;
 //
