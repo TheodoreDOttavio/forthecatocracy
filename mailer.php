499f9c3575293946cb71a584beb224f24386ca80
theodoredottavio <teddottavio@gmail.com>
2017-10-01 16:58:11 -0400

    phpmailer testing

--- mailer.php
+++ mailer.php
@@ -11,22 +11,24 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
   //https://github.com/PHPMailer/PHPMailer
-  $mail->SMTPDebug = 2;                        // Enable verbose debug output
+  $mail->SMTPDebug = 1;                        // Enable verbose debug output
   $mail->IsSMTP();
   $mail->CharSet = 'UTF-8';
 
   $mail->SMTPAuth = true;                               // Enable SMTP authentication
-  $mail->Host     = "teddottavio@gmail.com";            // SMTP server example
-  $mail->Username = 'teddottavio';                      // SMTP username
+  $mail->Host = "smtp.gmail.com";
+  $mail->Username = 'teddottavio@gmail.com';                      // SMTP username
   $mail->Password = getenv('gpass');                    // SMTP password
-  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-  $mail->Port = 25;                                     // TCP port to connect to
-  $mail->addAddress('7186785933@myboostmobile.com');    // Add a recipient
+  $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 465;                                    // TCP port to connect to (25 for gmail?)
+  $mail->SetFrom("teddottavio@gmail.com");
+  $mail->AddAddress("7186785933@myboostmobile.com");    // Add a recipient
+  $mail->AddAddress("teddottavio@gmail.com");
 
-  $mail->isHTML(false);                                  // Set email format to HTML
+  $mail->IsHTML(true);                                  // Set email format to HTML
   $mail->Subject = $_POST['clientPhone'];
   $mail->Body = $_POST['message'];
-  $mail->send();
+  $mail->Send();
   echo 'Message has been sent';
 } catch (Exception $e) {
   echo 'Message could not be sent.';
