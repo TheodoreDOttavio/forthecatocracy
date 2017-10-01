0cf0e3dd34058f9f517f197b4ad119729337aebf
theodoredottavio <teddottavio@gmail.com>
2017-10-01 16:36:11 -0400

    phpmailer testing

--- mailer.php
+++ mailer.php
@@ -11,19 +11,21 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
   //https://github.com/PHPMailer/PHPMailer
-  $mail = new PHPMailer();
+  $mail->SMTPDebug = 2;                        // Enable verbose debug output
   $mail->IsSMTP();
   $mail->CharSet = 'UTF-8';
 
-  $mail->Host       = "teddottavio@gmail.com"; // SMTP server example
-  $mail->SMTPDebug  = 0;                       // enables SMTP debug information (for testing)
-  $mail->SMTPAuth   = true;                    // enable SMTP authentication
-  $mail->Port       = 25;                      // set the SMTP port for the GMAIL server
-  $mail->Username   = "teddottavio";
-  $mail->Password   = getenv('gpass');
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Host     = "teddottavio@gmail.com";            // SMTP server example
+  $mail->Username = 'teddottavio';                      // SMTP username
+  $mail->Password = getenv('gpass');                    // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 25;                                     // TCP port to connect to
+  $mail->addAddress('7186785933@myboostmobile.com');    // Add a recipient
+
   $mail->isHTML(false);                                  // Set email format to HTML
   $mail->Subject = $_POST['clientPhone'];
-  $mail->AltBody = $_POST['message'];
+  $mail->Body = $_POST['message'];
   $mail->send();
   echo 'Message has been sent';
 } catch (Exception $e) {
